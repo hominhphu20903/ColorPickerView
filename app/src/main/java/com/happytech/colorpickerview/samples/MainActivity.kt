1a527b8517433f72f4b8b53b49c487510a2b9068
PhuHM <hominhphu20903@gmail.com>
2025-08-10 13:11:49 +0700

    fix: jitpack

--- app/src/main/java/com/happytech/colorpickerview/samples/MainActivity.kt
+++ app/src/main/java/com/happytech/colorpickerview/samples/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.colorPickerView.hue = binding.hueSlider
+        binding.colorPickerView.hueSliderView = binding.hueSlider
     }
 }
